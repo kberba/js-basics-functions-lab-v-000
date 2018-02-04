0f4e117e8819592f38e07770af400bb276d49d1a
Karen Berba <kberba@gmail.com>
2018-02-04 03:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function calculateVertical(start, destination) {
+  let vertical_dist;
+  vertical_dist = (start - destination) * 264;
+  return vertical_dist;
+}
+
+function distanceFromHqInBlocks(start) {
+  let blocks;
+  blocks = 42 - start;
+  if (blocks < 1) {
+    blocks = blocks * -1;
+    return blocks;
+  } else {
+    return blocks;
+  }
+}
+
+function distanceFromHqInFeet(num) {
+  let blocks;
+  blocks = distanceFromHqInBlocks(num);
+  let feet;
+  feet = blocks * 264;
+  return feet;
+}
+
+function distanceTravelledInFeet() {
+  
+}
